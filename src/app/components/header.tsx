e48059cb88dcac86d4ce077258c92bed1bf21812
Thamindu Liyanage <thaminduliyanage97@gmail.com>
2025-06-25 20:50:22 +0530

    Initialize Prisma with SQLite, add schema and basic queries

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -1,10 +1,20 @@
+import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 
 export default function Header() {
   return (
     <div className="border-black/10 border-b h-[40px] flex items-center justify-between px-5">
-      <div>Logo</div>
+      <Link href={"/"}>
+        <Image 
+         src="https://res.cloudinary.com/dx6ecncex/image/upload/f_auto,q_90/profile-6_kwgap2.png"
+         alt="logo"
+         width={30}
+         height={30}
+         className="rounded-full" 
+         priority
+         />
+      </Link>
       <ul className="flex items-center gap-3">
         <li>
           <Link href={"/"}>Home</Link>
--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -1,14 +1,25 @@
-import React from 'react'
+import React from "react";
+import { PrismaClient } from "@/generated/prisma";
+// name of the folder is the route
 
-export default function Page() {
+const prisma = new PrismaClient();
+export default async function Page() {
+  const posts = await prisma.post.findMany();
   return (
     <div className="text-center pt-12">
-      <h1 className="text-3xl capitalize font-bold mb-4">
-        Post
-      </h1>
+      <h1 className="text-3xl capitalize font-bold mb-4">Post</h1>
       <ul>
-        
+        {posts.map((post: { id: number; title: string }) => (
+          <li key={post.id} className="mb-2">
+            <a
+              href={`/posts/${post.id}`}
+              className="text-blue-500 hover:underline"
+            >
+              {post.title}
+            </a>
+          </li>
+        ))}
       </ul>
     </div>
-  )
+  );
 }
